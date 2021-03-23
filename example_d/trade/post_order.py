89eafbe3d082da626dc32a57adcbcbc2bbf663a4
LaithDaqqa <69797001+LaithDaqqa@users.noreply.github.com>
2021-03-23 22:12:21 +0300

    Update post_order.py
    help, add extra orders

--- example_d/trade/post_order.py
+++ example_d/trade/post_order.py
@@ -7,4 +7,9 @@ request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key)
 # print(request_client.change_position_mode(dualSidePosition=True))
 result = request_client.post_order(symbol="btcusd_200925", side=OrderSide.BUY, ordertype=OrderType.LIMIT, price=10001, quantity=2, timeInForce=TimeInForce.GTC)
 
+# these 2 examples to send limit buy or sell order
+#result = request_client.post_order(symbol="LTCUSDT", side=OrderSide.BUY, ordertype=OrderType.LIMIT, price=100, quantity=0.5, timeInForce=TimeInForce.GTC)
+#result = request_client.post_order(symbol="LTCUSDT", side=OrderSide.SELL, ordertype=OrderType.LIMIT, price=250, quantity=2, timeInForce=TimeInForce.GTC)
+
+
 PrintBasic.print_obj(result)
